cb206702f585ded485c1f5bd463b46816944f635
tuan <phananhtuan382017@gmail.com>
2020-06-11 10:45:37 +0700

    add folder app

---
+++ src/navigator/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+const AppContainer = () => {
+    return (
+        <NavigationContainer>
+            {/* APP */}
+        </NavigationContainer>
+    )
+}
+
+export default AppContainer
