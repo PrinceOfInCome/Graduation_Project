378266fee78f5e49d001bda818d47d6b6151d3f1
tuan <phananhtuan382017@gmail.com>
2020-06-12 09:07:09 +0700

    add folder

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {StyleSheet} from 'react-native';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store/index';
 import AppContainer from './src/navigator/index';
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <Provider store={store}>
