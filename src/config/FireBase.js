c472aae5eee82ef38e5a2cd3274ff01bc3138fe3
tuan <phananhtuan382017@gmail.com>
2020-06-15 10:22:07 +0700

    fix firebase

---
+++ src/config/FireBase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC5Sg4zD9-I6GpJeZBP3EaszxVzU8JkOsU",
+    authDomain: "travelapp-deb06.firebaseapp.com",
+    databaseURL: "https://travelapp-deb06.firebaseio.com",
+    projectId: "travelapp-deb06",
+    storageBucket: "travelapp-deb06.appspot.com",
+    messagingSenderId: "77323621470",
+    appId: "1:77323621470:web:a933cbe6cd7b26195310cf",
+    measurementId: "G-HYS9M212Y6"
+  };
+
+  export const firebaseApp = firebase.initializeApp(config);
